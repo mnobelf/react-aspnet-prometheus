46898215a83cbd35a8ef00e794bf6011ac107b8b
Nobelf <13517042@std.stei.itb.ac.id>
2022-09-18 10:49:32 +0700

    init

---
+++ react-asp.net-prometheus/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+    static displayName = Home.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <p>Simple metrics implementation using react front-end.</p>
+            </div>
+        );
+    }
+}
